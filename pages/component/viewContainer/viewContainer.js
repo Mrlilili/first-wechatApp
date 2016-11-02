e8d371c13de1e2feb9efcafa79207c56845b8bab
liyan <liyan1@yy.com>
2016-11-02 16:18:33 +0800

    增加视图容器demo

--- pages/component/viewContainer/viewContainer.js
+++ pages/component/viewContainer/viewContainer.js
@@ -1,20 +1,36 @@
+var order = ['red', 'yellow', 'blue', 'green', 'red'];
+
 Page({
-  data:{
-    text:"Page viewContainer"
+  data: {
+    toView: 'red',
+    scrollTop: 100
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
-  }
+  },
+  tapMove: function () {
+    console.log('move');
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
 })
