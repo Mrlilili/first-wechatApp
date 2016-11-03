4bf214ad4c8be5126d3f7b4274d698da826292a2
liyan <liyan1@yy.com>
2016-11-03 17:32:29 +0800

    增加组件-基础内容

--- pages/component/viewContainer/viewContainer.js
+++ pages/component/viewContainer/viewContainer.js
@@ -3,7 +3,18 @@ var order = ['red', 'yellow', 'blue', 'green', 'red'];
 Page({
   data: {
     toView: 'red',
-    scrollTop: 100
+    scrollTop: 100,
+    swiperConfig: {
+      imgUrls: [
+        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ],
+      indicatorDots: false,
+      autoplay: false,
+      interval: 5000,
+      duration: 1000
+    }
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
